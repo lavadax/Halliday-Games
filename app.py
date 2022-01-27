f73f528864a97480c11d423da954f785fe30eed2
Kevin Schepers <gamer.mmo@gmail.com>
2022-01-27 23:31:11 +0000

    Add initial route for new review page

--- app.py
+++ app.py
@@ -38,6 +38,11 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/add_review")
+def add_review():
+    return render_template("add_review.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
