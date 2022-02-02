7d54ad1d4a1568ab531cdd449b2ca207836b528d
Kevin Schepers <gamer.mmo@gmail.com>
2022-02-02 08:37:49 +0000

    Add index search for game_title

--- app.py
+++ app.py
@@ -50,9 +50,9 @@ def my_reviews(user_id):
 def search():
     if request.method == "POST":
 
-        game_title = request.form.get("game_title")
+        query = request.form.get("game_title")
         reviews = list(mongo.db.reviews.find(
-            {"game_title": game_title}).sort("score", -1))
+            {"$text": {"$search": query}}).sort("score", -1))
         return render_template("search.html", reviews=reviews)
 
     reviews = list(mongo.db.reviews.find().sort("score", -1))
